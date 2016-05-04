05b267ca90f3224c12a298ab5ceea645f29a9605
jon0410 <jon0410@hotmail.com>
2016-05-04 23:48:51 +1000

    Day 16

---
+++ Day 16/SPPM/Form3.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(12, 12)
+        Me.Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(415, 300)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 318)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 32)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Show Graph"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(333, 318)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 32)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Back"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(455, 384)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ Day 16/SPPM/Form3.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form3
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Form4.Show()
+        Me.Hide()
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        
+
+        '  Dim Conn As OleDbConnection = New OleDbConnection
+
+        '     Conn = New System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='" & Form1.filePath & "';Extended Properties=Excel 8.0;")
+        '   Conn.Open()
+        '   Dim cmd As OleDbCommand = New OleDbCommand("SELECT [Date], [TotalSales] FROM [sheet3$]", Conn)
+        '   Dim dr As OleDbDataReader = cmd.ExecuteReader
+        '   While dr.Read
+        'Chart1.Series("sheet3$").Points.AddXY(dr("Date").ToString, dr("TotalSales").ToString)
+        '   End While
+        '   dr.Close()
+        'Reference: http://www.visual-basic-tutorials.com/display-data-as-charts-and-graph-in-visual-basic.html#sthash.aRIaIOEN.dpuf
+
+        Dim lLastRow As Integer
+        Dim dates() As String
+        Dim total() As Integer
+        Dim count As Integer
+        Dim x As Integer
+
+        dates = {0}
+        total = {0}
+        count = 0
+        Form1.App = New Microsoft.Office.Interop.Excel.Application()
+        Form1.workbook = Form1.App.Workbooks.Open(Form1.filePath)
+
+        Form1.worksheet = Form1.workbook.Worksheets("sheet3")
+
+        With Form1.worksheet
+            'find the last row of the list
+            lLastRow = Form1.worksheet.Cells(Form1.worksheet.Rows.Count, "H").End(Excel.XlDirection.xlUp).Row
+            'shift from an extra row if list has header
+
+
+        End With
+
+        For x = 2 To lLastRow
+            If String.IsNullOrEmpty(Form1.worksheet.Cells(x, 8).Value) Then
+            Else
+                count = count + 1
+            End If
+        Next
+
+        
+
+
+        
+
+        For x = 2 To lLastRow
+
+            If String.IsNullOrEmpty(Form1.worksheet.Cells(x, 8).Value) Then
+            Else
+                Array.Resize(dates, dates.Length + 1)
+                dates(dates.Length - 1) = Form1.worksheet.Cells(x, 8).value
+
+                Array.Resize(total, total.Length + 1)
+                total(total.Length - 1) = Form1.worksheet.Cells(x, 9).value.ToString
+            End If
+
+            ' dates() = Form1.worksheet.Cells(lLastRow, 8).value.ToString
+
+
+        Next
+
+        Chart1.Series(0).Points.DataBindXY(dates, total)
+
+
+
+        
+
+        Form1.workbook.Save()
+        Form1.workbook.Close()
+        Form1.App.Quit()
+
+        Form1.releaseObject(Form1.worksheet)
+        Form1.releaseObject(Form1.workbook)
+
+        Form1.releaseObject(Form1.App)
+
+
+
+
+    End Sub
+
+
+    
+
+    Private Sub Form2_Closing(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        Button2.PerformClick()
+
+        Form1.Button4.PerformClick()
+        LoginForm1.Enabled = True
+        LoginForm1.CancelButton.PerformClick()
+
+
+
+    End Sub
+End Class
